78c7113aeaf557fd8d13dbae943739485f7e621e
John <wei.jiang@winsuntech.cn>
2016-03-23 09:55:44 +0800

    Update CreateFilesFolders.sh

--- Tools/CreateFilesFolders.sh
+++ Tools/CreateFilesFolders.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-for((i=1;i<=500;i++))
-do
-    num=`date +%s%N`
-    hello=$[num%2]
+# Create files & folders randomly with specific path & number
+# $1: path for whole files & folders root folder
+# $2: the amount of whole files & folders
 
-    if [ ${hello} -eq 0 ]
+path=`pwd`
+
+mkdir -p $1
+cd $1
+
+for((i=1;i<=$2;i++))
+do
+    randomNum=`date +%s%N`
+    swtich=$[randomNum%2]
+
+    if [ ${swtich} -eq 0 ]
     then
-        echo $num > $num.txt
+        echo $randomNum > $randomNum.txt
     else
-        mkdir $num
+        mkdir $randomNum
     fi
 done
+
+chmod 777 -R *
+
+cd $path
 
 
