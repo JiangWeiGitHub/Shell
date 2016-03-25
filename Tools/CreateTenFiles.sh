4385c15880a6dfed075e5f38846e63bb3c72e8c3
John <wei.jiang@winsuntech.cn>
2016-03-25 10:41:28 +0800

    Create CreateTenFiles.sh

---
+++ Tools/CreateTenFiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+path=`pwd`
+
+mkdir -p $1
+cd $1
+mkdir -p 1 2 3
+touch 4.txt
+echo "4.txt" > 4.txt
+
+cd 1
+mkdir 5
+touch 6.txt
+echo "6.txt" > 6.txt
+
+cd 5
+mkdir 7
+touch 8.txt
+echo "8.txt" > 8.txt
+
+cd ../..
+
+cd 2
+mkdir 9
+touch 10.txt
+echo "10.txt" > 10.txt
+
+cd ..
+
+cd $path
+chmod 777 -R *
